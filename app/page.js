95bfcb4d319e89bda9be6a7bbb02b60c3ff2aec3
Hossam Cj <hossamcj@gmail.com>
2023-09-26 22:19:50 +0300

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,29 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<div className="container">
+			<h1 className="text-center">Hello En: Mostafa Yaser</h1>
+			<div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+				<p>
+					Welcome to my Next.js 13 app! I've used Next.js's
+					advanced routing features to create a smooth user experience.
+					Our app structure is streamlined, thanks to the root directory approach.
+				</p>
+			</div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+			<div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+				<p>
+				But that's not all. We've integrated JSONPlaceholder to display a list of posts. Access them via the 'Posts' link in the navbar. Explore post titles and descriptions, and click any title to view full post details. The post's unique ID is included in the URL for precision.
+				</p>
+			</div>
+			<div class="position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill">
+				<p>
+				In summary, our app highlights Next.js routing efficiency, simple structure, and seamless navigation, all while showcasing posts from JSONPlaceholder.
+				</p>
+			</div>
+			
+		</div>
+	);
 }
